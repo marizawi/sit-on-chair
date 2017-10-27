20c95feda879cc3a0f074fd89ffeb968f3253426
Maria Zawiazalec <zawiaz@gmail.com>
2017-10-27 23:19:36 +0200

    RWD tbc

--- script.js
+++ script.js
@@ -8,11 +8,26 @@ document.addEventListener("DOMContentLoaded", function(){
         });
     }
 
-    for (var i=0; i<chairs.length; i++) {
-        chairs[i].addEventListener("mouseleave", function() {
-            this.style.display = "block";
-        });
-    }
+    // for (var i=0; i<chairs.length; i++) {
+    //     chairs[i].addEventListener("mouseleave", function() {
+    //         this.style.display = "block";
+    //     });
+    // }
+
+    var info1 = document.querySelector(".info1");
+    var info2 = document.querySelector(".info2");
+
+    info1.addEventListener("mouseleave", function(){
+        for (var i=0; i<chairs.length; i++) {
+            chairs[i].style.display = "block";
+        }
+    })
+
+    info2.addEventListener("mouseleave", function(){
+        for (var i=0; i<chairs.length; i++) {
+            chairs[i].style.display = "block";
+        }
+    })
 
 
 
@@ -59,6 +74,70 @@ document.addEventListener("DOMContentLoaded", function(){
         listElements[current].classList.add("visible");
 
     })
+
+// CALCULATOR
+
+    var dropdown = document.querySelectorAll('.drop_down_list');
+    var liEl = document.querySelectorAll('.list_panel > li');
+    var counter = 0;
+    var sum = document.querySelector('.sum');
+
+    var title = document.querySelector('.panel_left .title');
+    var color = document.querySelector('.panel_left .color');
+    var pattern = document.querySelector('.panel_left .pattern');
+    var titleValue = document.querySelector('.panel_right .title.value');
+    var colorValue = document.querySelector('.panel_right .color.value');
+    var patternValue = document.querySelector('.panel_right .pattern.value');
+
+    var transportInput = document.querySelector('.checkbox input');
+    var transport = document.querySelector('.panel_left .transport');
+    var transportValue = document.querySelector('.panel_right .transport.value');
+
+
+
+    dropdown.forEach(function (el) {
+        el.addEventListener('click', function () {
+            if (this.children) {
+                this.lastElementChild.classList.toggle("block");
+            }
+        });
+    });
+
+    liEl.forEach(function (el) {
+        function calc(name, value) {
+            name.innerText = el.innerText;
+            value.innerText = el.dataset.price + "zł";
+            counter += parseInt(el.dataset.price);
+            sum.innerText = counter + "zł";
+        };
+
+        el.addEventListener('click', function () {
+            var label = this.parentElement.parentElement.firstElementChild;
+            label.innerText = el.innerText;
+            label.style.color = "black";
+            if (this.parentElement.parentElement === dropdown[0]) {
+                calc(title, titleValue);
+            } else if (this.parentElement.parentElement === dropdown[1]) {
+                calc(color, colorValue);
+            } else if (this.parentElement.parentElement === dropdown[2]) {
+                calc(pattern, patternValue);
+            }
+        });
+    });
+
+    transportInput.addEventListener("click", function () {
+        if (transportInput.checked === true) {
+            transport.innerText = "Transport";
+            transportValue.innerText = transportInput.dataset.transportPrice + "zł";
+            counter += parseInt(transportInput.dataset.transportPrice);
+        }
+        else {
+            transport.innerText = "";
+            transportValue.innerText = "";
+            counter -= parseInt(transportInput.dataset.transportPrice);
+        }
+        sum.innerText = counter + "zł";
+    });
 
 
 
